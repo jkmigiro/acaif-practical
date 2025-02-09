ec7e39b1d525ab42dfcebbbc283b269cb4f534de
jkmigiro <jkmigiro>
2025-02-10 01:19:54 +0300

    Port number

--- acaif-backend/src/main.ts
+++ acaif-backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT ?? 443);
 }
 bootstrap();
 export default bootstrap;
