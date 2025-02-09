8655559b571b299b00e26670d1fa6059e210a55d
jkmigiro <jkmigiro>
2025-02-10 02:09:35 +0300

    Whitelist

--- acaif-backend/src/main.ts
+++ acaif-backend/src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*", 
+    origin: ["https://acaif-frontend.vercel.app"], 
     methods: "GET,POST,PUT,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type, Authorization",
+    credentials: true
   });
-  await app.listen(process.env.PORT ?? 443);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
 export default bootstrap;
